e0656409ecfe6802116c3c8e915f0ec761e02c2d
alexm-gh <alexchatgpt.acg@gmail.com>
2026-03-26 21:45:35 +0300

    dev branch

--- bot/config.py
+++ bot/config.py
@@ -3,6 +3,7 @@
 Reads secrets from .env.bot.secret file.
 """
 
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -13,12 +14,13 @@ class Settings(BaseSettings):
         env_file=".env.bot.secret",
         env_file_encoding="utf-8",
         extra="ignore",
+        populate_by_name=True,
     )
 
-    bot_token: str = ""
-    lms_api_base_url: str = "http://localhost:42002"
-    lms_api_key: str = ""
-    llm_api_key: str = ""
+    bot_token: str = Field("", alias="BOT_TOKEN")
+    lms_api_base_url: str = Field("http://localhost:42002", alias="LMS_API_BASE_URL")
+    lms_api_key: str = Field("", alias="LMS_API_KEY")
+    llm_api_key: str = Field("", alias="LLM_API_KEY")
 
 
 settings = Settings()
